96d5bd63ce46df8f6298751389c099570145fc65
SaiCharan87s <pickraju24@gmail.com>
2021-09-25 17:06:31 +0530

    this is the pom file commit

--- BusProjectMain/src/main/java/com/lti/model/User.java
+++ BusProjectMain/src/main/java/com/lti/model/User.java
@@ -1,5 +1,31 @@
 package com.lti.model;
 
-public class User {
+import java.util.Date;
 
+import javax.persistence.Column;
+import javax.persistence.Id;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+public class User {
+	@Id
+	@Column(name="registered_email")
+	private String registeredEmail;
+	
+	@Column(name="user_name")
+	private String userName;
+	
+	@Column(name="gender")
+	private String gender;
+	
+	@Column(name="phone")
+	private String phoneNumber;
+	
+	@Column(name="password")
+	private String password;
+	
+	
+	@Column(name="DOB")
+	@JsonFormat(shape=JsonFormat.Shape.STRING, pattern="yyyy-MM-dd")
+	private Date dateOfBirth;
 }
